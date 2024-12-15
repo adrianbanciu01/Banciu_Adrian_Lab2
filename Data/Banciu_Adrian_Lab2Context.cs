c5006eab42d8a2335ceee89d22a1b17a785907fd
adrianbanciu01 <banciu01@gmail.com>
2024-12-15 22:55:28 +0200

    Add project files.

---
+++ Data/Banciu_Adrian_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Banciu_Adrian_Lab2.Models;
+
+namespace Banciu_Adrian_Lab2.Data
+{
+    public class Banciu_Adrian_Lab2Context : DbContext
+    {
+        public Banciu_Adrian_Lab2Context (DbContextOptions<Banciu_Adrian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Banciu_Adrian_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Banciu_Adrian_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
